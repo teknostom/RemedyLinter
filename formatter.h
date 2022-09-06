75487297d88f101dcc75565f49faab3a9804f4bf
teknostompa <evaldsson.tom@gmail.com>
2022-09-06 16:05:48 +0200

    FIrst commit with basic project planning.

---
+++ formatter.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "fstream"
+#include "string"
+
+class formatter{
+    formatter();
+    void formatFile(std::string file);
+};
---
+++ rules.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "fstream"
+#include "string"
+
+class rules{
+    rules(std::string file);
+};
